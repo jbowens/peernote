28d552bb63fd52a1ba9d0c4da9601cea269c8962
Alec Lee <alec.lee15@gmail.com>
2014-02-13 21:49:32 -0500

    less and shit

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask, g, request, render_template
+from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__)
+app.debug = True
+assets = Environment(app)
+
+common_css = Bundle(
+    "less/*.less",
+    filters="less,cssmin",
+    output="gen/dist.css",
+)
+
+assets.register('common_css', common_css)
 
 @app.route('/')
 def index():
