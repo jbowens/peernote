8b34896a15ef05befea854ef7b2d84a19a612b00
Alec Lee <alec.lee15@gmail.com>
2014-02-15 00:29:47 -0500

    bundles cleaner

--- app/bundles.py
+++ app/bundles.py
@@ -3,19 +3,20 @@ from app import app
 
 assets = Environment(app)
 
-base_css = Bundle(
-    "scss/bootstrap.scss",
-    "scss/reset.scss",
-    "scss/top_nav.scss",
-    filters="scss,cssmin",
-    output="gen/base.css",
+assets.register('base_css',
+    Bundle(
+        "scss/bootstrap.scss",
+        "scss/reset.scss",
+        "scss/top_nav.scss",
+        filters="scss,cssmin",
+        output="gen/base.css",
+    )
 )
 
-home_css = Bundle(
-    "scss/home.scss",
-    filters="scss,cssmin",
-    output="gen/home.css",
+assets.register('home_css',
+    Bundle(
+        "scss/home.scss",
+        filters="scss,cssmin",
+        output="gen/home.css",
+    )
 )
-
-assets.register('base_css', base_css)
-assets.register('home_css', home_css)
