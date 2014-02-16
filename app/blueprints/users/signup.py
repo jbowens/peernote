fe771d0ebfa8cb419d9db12032cbd7b9c12dbdad
Jackson Owens <jackson_owens@brown.edu>
2014-02-15 22:38:57 -0500

    check that usernames and emails are unique

--- app/blueprints/users/signup.py
+++ app/blueprints/users/signup.py
@@ -2,13 +2,26 @@ from flask import render_template, request, redirect, url_for, session, current_
 from flask_wtf import Form
 from wtforms import TextField, PasswordField
 from wtforms.validators import *
+from wtforms.validators import ValidationError
 from app.models.user import User
 from app.blueprints.users import users
 from app import db
 
+def email_uniqueness(form, field):
+    user = User.query.filter_by(email=field.data).first()
+    if user:
+        raise ValidationError('That email is already registered.')
+
+def username_uniqueness(form, field):
+    user = User.query.filter_by(username=field.data).first()
+    if user:
+        raise ValidationError('That username is already registered.')
+
 class SignUpForm(Form):
-    username = TextField('username', validators=[DataRequired(), Length(max=30)])
-    email = TextField('email', validators=[DataRequired(), Email()])
+    username = TextField('username', validators=[DataRequired(),
+                                                 Length(max=30),
+                                                 username_uniqueness])
+    email = TextField('email', validators=[DataRequired(), Email(), email_uniqueness])
     pw = PasswordField('pw', validators=[DataRequired()])
     pw_again = PasswordField('pw_again', validators=[DataRequired(), EqualTo('pw')])
 
