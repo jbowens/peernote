1e652149dac3dc68391bba32f389dfba5e2315c5
Jackson Owens <jackson_owens@brown.edu>
2014-02-16 03:18:29 -0500

    slightly less dumb /settings page

--- app/blueprints/users/settings.py
+++ app/blueprints/users/settings.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app.blueprints.users import users
 from app.decorators import login_required
 
 @users.route('/settings', methods=['GET','POST'])
 @login_required
 def settings():
-    return 'lolol'
+    return render_template('settings.html')
