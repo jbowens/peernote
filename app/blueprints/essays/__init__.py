e57c03dcde242da62da27f1b6816916bf2e82e10
Jackson Owens <jackson_owens@brown.edu>
2014-02-16 01:41:41 -0500

    started of /essays index

--- app/blueprints/essays/__init__.py
+++ app/blueprints/essays/__init__.py
@@ -6,6 +6,7 @@ from flask import Blueprint
 essays = Blueprint('essays', __name__)
 
 # Import the endpoints.
+from index import essays_index
 from create import create_essay
 from review import review_essay
 from upload import upload_essay
---
+++ app/blueprints/essays/index.py
@@ -0,0 +1,10 @@
+from flask import request, render_template, g
+from app.decorators import login_required
+from app.blueprints.essays import essays
+from app.models.essay import Essay
+
+@essays.route('/')
+@login_required
+def essays_index():
+    essays = Essay.query.filter_by(uid=g.user.uid).all()
+    return render_template('essays/index.html', essays=essays)
