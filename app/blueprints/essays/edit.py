af41663b251f8523c29cf959884ce1c736e5051c
Alec Lee <alec.lee15@gmail.com>
2014-02-16 13:15:01 -0500

    secure systems :hurtrealbad:

--- app/blueprints/essays/edit.py
+++ app/blueprints/essays/edit.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, redirect, url_for
+from flask import request, render_template, redirect, url_for, g
 from app.blueprints.essays import essays
 from app.models.essay import Essay
 from app.decorators import login_required
@@ -7,7 +7,7 @@ from app.decorators import login_required
 @login_required
 def edit_essay(essayid):
     essay = Essay.query.filter_by(eid=essayid).first()
-    if essay:
+    if essay and g.user.uid == essay.uid:
         return render_template('essays/editor.html', current_essay=essay)
     else:
         return redirect(url_for('essays.essays_index'))
