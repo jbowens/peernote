526a8d74ff10184d01ff22633f2056eddd1b19fb
Jackson Owens <jackson_owens@brown.edu>
2014-02-15 22:59:17 -0500

    unique constraint on username

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,7 @@ from app import db
 
 class User(db.Model):
     uid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30))
+    username = db.Column(db.String(30), unique=True)
     email = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(128))
 
