a1c66b9e448ea9fc902aeceae2110b57a51329fc
Jackson Owens <jackson_owens@brown.edu>
2014-02-15 21:36:34 -0500

    flashes for /log-in

--- app/blueprints/users/login.py
+++ app/blueprints/users/login.py
@@ -1,11 +1,10 @@
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template, request, redirect, url_for, session, flash
 from app.models.user import User
 from app.blueprints.users import users
 from app import db
 
 @users.route('/log-in', methods=['GET','POST'])
 def log_in():
-    login_failed = False
     if request.method == 'POST':
         # They submitted the form and we should process it.
         
@@ -15,6 +14,6 @@ def log_in():
         if user and user.check_password(request.form.get('password', '')):
             session['uid'] = user.uid
             return redirect('/')
-        login_failed = True
+        flash('No user with the given username and password combination was found.')
         
-    return render_template('log_in.html', login_failed=login_failed)
+    return render_template('log_in.html')
