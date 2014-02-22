920a1e8dec0e04d7b65cfc0b13c9e7ab4a1a9d28
Jackson Owens <jackson_owens@brown.edu>
2014-02-22 14:30:14 -0500

    fixed essay saving and white space

--- app/static/js/essays/editor.js
+++ app/static/js/essays/editor.js
@@ -10,16 +10,35 @@ peernoteNS.essays.AUTOSAVE_PAUSE_MILLIS = 2500;
  */
 peernoteNS.autosave_timer = null;
 
+/**
+ * Extracts the text of the essay from the editor. This is necessary in order
+ * to properly handle the different ways content-editable input can appear.
+ * Most content will be in a <div> or <p> but some will be floating just as
+ * a text node.
+ */
+peernoteNS.essays.extractText = function() {
+  var children = $('.editor-background .content')[0].childNodes;
+  var lines = [];
+  for (var i = 0; i < children.length; i++) {
+    if (children[i].nodeType == 3) {
+      lines.push(children[i].textContent);
+    }
+    else if (children[i].nodeType == 1 && children[i].tagName != 'H1') {
+      lines.push($(children[i]).text());
+    }
+  }
+
+  var text = lines.join('\n');
+  return text; 
+};
+
+/**
+ * Saves the draft to the db via ajax.
+ */
 peernoteNS.essays.save = function() {
   var $title = $('#essay-title');
-  var $text = $('.editor-background .content p, .editor-background .content div, .essay-text');
   var title = $title.text();
-
-  var lines = [];
-  $text.each(function() {
-    lines.push($(this).text());
-  });
-  var text = lines.join('\n');
+  var text = peernoteNS.essays.extractText();
 
   var params = {
     title: title,
@@ -41,6 +60,14 @@ peernoteNS.essays.save = function() {
   }
 };
 
+peernoteNS.essays.keydown = function(e) {
+  // We want tabs to be treated as a literal tab characters,
+  // not for navigation.
+  if (e.keyCode == 9) {
+    e.preventDefault();
+  }
+};
+
 peernoteNS.essays.keystroke = function(e) {
   if (peernoteNS.autosave_timer) {
     clearTimeout(peernoteNS.autosave_timer);
@@ -55,6 +82,7 @@ peernoteNS.essays.keystroke = function(e) {
 
 peernoteNS.essays.initEditor = function() {
   $('.page-container').keypress(peernoteNS.essays.keystroke);
+  $('.page-container .content').keydown(peernoteNS.essays.keydown);
 };
 
 $(document).ready(function(e) {
