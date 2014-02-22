f308512012e3685426bc8ddc5e0f28aaddc4b5f7
Jackson Owens <jackson_owens@brown.edu>
2014-02-22 17:26:56 -0500

    email signup on splash page

--- app/models/email.py
+++ app/models/email.py
@@ -1,5 +1,10 @@
+from datetime import datetime
 from app import db
 
 class Email(db.Model):
     eid = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
+    signup_date = db.Column(db.DateTime)
+
+    def __init__(self):
+      self.signup_date = datetime.utcnow()
