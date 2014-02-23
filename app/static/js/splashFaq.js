793b446a9282cc8669740d9ae649264966429fce
bryceAebi <bryce_aebi@brown.edu>
2014-02-22 20:33:41 -0500

    updating dat sploosh

--- app/static/js/splashFaq.js
+++ app/static/js/splashFaq.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     /*** show and hide faq answers ***/
-    $("#faq-1").click(function() { showHide($("#faq-a-1"), $("#faq-1 i"), $('#faq-1 h3')); });
+/*    $("#faq-1").click(function() { showHide($("#faq-a-1"), $("#faq-1 i"), $('#faq-1 h3')); });
     $("#faq-2").click(function() { showHide($("#faq-a-2"), $("#faq-2 i"), $('#faq-2 h3')); });
     $("#faq-3").click(function() { showHide($("#faq-a-3"), $("#faq-3 i"), $('#faq-3 h3')); });
     $("#faq-4").click(function() { showHide($("#faq-a-4"), $("#faq-4 i"), $('#faq-4 h3')); });
@@ -9,7 +9,7 @@ $(document).ready(function() {
     $("#faq-6").click(function() { showHide($("#faq-a-6"), $("#faq-6 i"), $('#faq-6 h3')); });
     $("#faq-7").click(function() { showHide($("#faq-a-7"), $("#faq-7 i"), $('#faq-7 h3')); });
     $("#faq-8").click(function() { showHide($("#faq-a-8"), $("#faq-8 i"), $('#faq-8 h3')); });
-
+*/
     function showHide($displayTarget, $i, $h3) {
         if ($displayTarget.is(":visible")){
             $displayTarget.hide("slide", {direction: "up"}, 200);
