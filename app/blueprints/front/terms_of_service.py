d608a5a7f5f74d1d7dd58e64f218cf0d24bfb78b
bryceAebi <bryce_aebi@brown.edu>
2014-02-25 21:53:37 -0500

    removing unnecessary include

--- app/blueprints/front/terms_of_service.py
+++ app/blueprints/front/terms_of_service.py
@@ -1,6 +1,5 @@
 from flask import render_template, g
 from app.blueprints.front import front
-from app.blueprints.users.profile import show_user_profile
 
 @front.route('/terms-of-service', methods=['GET'])
 def terms_of_service():
