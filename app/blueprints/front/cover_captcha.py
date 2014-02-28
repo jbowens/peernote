5386e382e6aaeaa9fe91f4e68a000de7dc9996f3
Jackson Owens <jackson_owens@brown.edu>
2014-02-28 13:00:06 -0500

    captcha is actually hard to beat now

--- app/blueprints/front/cover_captcha.py
+++ app/blueprints/front/cover_captcha.py
@@ -1,7 +1,9 @@
-import random, json
-from flask import Response, session, current_app
+import random, json, Image
+from cStringIO import StringIO
+from flask import Response, session, current_app, send_file
 from app.blueprints.front import front
 from app.app_root import APP_ROOT
+from app.captcha.distortions import SineWarp
 
 @front.route('/captcha.png', methods=['GET'])
 def captcha_image():
@@ -16,8 +18,19 @@ def captcha_image():
     img_loc = APP_ROOT + '/captcha/' + img[0]
     session['captcha_title'] = img[1]
 
-    with open(img_loc) as f:
-        img_data = f.read()
-        resp = Response(img_data, mimetype='image/png')
-        resp.headers['Cache-Control'] = 'no-cache'
-        return resp
+    imgobj = Image.open(img_loc)
+    operation = random.randint(0,1)
+    if operation == 0:
+        # Grayscale the image
+        imgobj = imgobj.convert('L')
+
+    # Now perform a distortion
+    warp = SineWarp()
+    imgobj = warp.render(imgobj)
+
+    img_io = StringIO()
+    imgobj.save(img_io, 'png')
+    img_io.seek(0)
+    resp = Response(img_io.read(), mimetype='image/png')
+    resp.headers['Cache-Control'] = 'no-cache'
+    return resp
