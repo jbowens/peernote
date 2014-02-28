8cb281b12ac0d6aba7f792f1f985ef771a7400e6
Jackson Owens <jackson_owens@brown.edu>
2014-02-28 18:20:07 -0500

    url keywords

--- app/blueprints/users/profile.py
+++ app/blueprints/users/profile.py
@@ -3,9 +3,9 @@ from app.models.user import User
 from app.blueprints.users import users
 from app.decorators import login_required
 
-@users.route('/users/<uid>', methods=['GET','POST'])
-def show_user_profile(uid):
-    user = User.query.filter_by(uid=uid).first()
+@users.route('/users/<keyword>', methods=['GET','POST'])
+def show_user_profile(keyword):
+    user = User.query.filter_by(url_keyword=keyword).first()
 
     if not user:
         abort(404)
