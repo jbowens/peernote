8f0e386fb9e54e7e0d2c3a9eddfcc5595b96c273
Jackson Owens <jackson_owens@brown.edu>
2014-03-01 12:47:37 -0500

    oops

--- app/models/email.py
+++ app/models/email.py
@@ -4,7 +4,4 @@ from app import db
 class Email(db.Model):
     eid = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
-    signup_date = db.Column(db.DateTime)
-
-    def __init__(self):
-      self.signup_date = datetime.now
+    signup_date = db.Column(db.DateTime, default=datetime.now)
