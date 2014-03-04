1ceed2f7ffac879012cb50fc87178be558288428
Jackson Owens <jackson_owens@brown.edu>
2014-03-04 00:24:31 -0500

    reworked review endpoint, added check for deleted essay

--- app/blueprints/reviews/reviewer.py
+++ app/blueprints/reviews/reviewer.py
@@ -1,15 +1,26 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, abort, current_app
 from app import db
 from app.blueprints.reviews import reviews
 from app.models.review import Review
 from app.models.draft import Draft
+from app.models.essay import Essay
 
 @reviews.route('/<reviewhash>', methods=['GET'])
 def review_draft(reviewhash):
     review = Review.query.filter_by(urlhash=reviewhash).first()
-    if review:
-        current_draft = Draft.query.filter_by(did=review.did).first()
-        if current_draft:
-            return render_template('reviews/reviewer.html', current_draft=current_draft)
 
-    return redirect(url_for('essays.essays_index')) # TODO: something else
+    if not review:
+        abort(404)
+
+    draft = Draft.query.filter_by(did=review.did).first()
+    if not draft:
+        # This should never happen. We don't actually delete the drafts from the database.
+        current_app.logger.error('Reviewer had valid review hash for nonexistent draft.')
+        abort(404)
+
+    essay = Essay.query.get(draft.eid)
+    if essay.deleted:
+        # TODO: Make this error page prettier.
+        return 'Sorry, that essay has been deleted.'
+
+    return render_template('reviews/reviewer.html', current_draft=draft)
