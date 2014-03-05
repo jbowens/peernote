4c08fbb545833f6e828ae9df4667f1573955c2b4
Alec Lee <alec.lee15@gmail.com>
2014-03-05 17:44:04 -0500

    recent docs on profile

--- app/blueprints/users/profile.py
+++ app/blueprints/users/profile.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, current_app, abort
 from app.models.user import User
+from app.models.essay import Essay
 from app.blueprints.users import users
 from app.decorators import login_required
 
@@ -10,4 +11,11 @@ def show_user_profile(keyword):
     if not user:
         abort(404)
 
-    return render_template('users/profile.html', viewed_user=user, page_title="Dashboard",nav_extra="dashboard")
+    essays = Essay.query.filter_by(uid=user.uid, deleted=False).order_by(Essay.modified_date.desc())[:5]
+
+    return render_template('users/profile.html',
+        viewed_user=user,
+        recent_essays=essays,
+        page_title="Dashboard",
+        nav_extra="dashboard"
+    )
