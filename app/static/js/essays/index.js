a62055daa49f7139d48d0f3fdf686414740d1da7
Alec Lee <alec.lee15@gmail.com>
2014-03-06 18:51:34 -0500

    widgetified docs

--- app/static/js/essays/index.js
+++ app/static/js/essays/index.js
@@ -1,25 +1,5 @@
 var peernoteNS = peernoteNS || {};
 
-peernoteNS.essays.initTrashButton = function() {
-  for (var i = 0; i < peernoteNS.essays.essayIds.length; i++) {
-
-    (function (i) {
-      $('.trash-essay-' + i).click(function() {
-        var eid = peernoteNS.essays.essayIds[i];
-        $.post('/api/essays/delete', {eid:eid, csrf:peernoteNS.csrf}, function(data) {
-          if (data.status == "success") {
-            // TODO: not very elegant
-            $('.trash-essay-' + i).parent().remove();
-          } else {
-            console.log(data.error);
-          }
-        });
-      });
-    })(i); // L O L JAVASCRIPT SCOPING
-
-  }
-};
-
 $(document).ready(function() {
-    peernoteNS.essays.initTrashButton();
+  peernoteNS.widgets.essaysList.init($('#essays-list-container'), {});
 });
