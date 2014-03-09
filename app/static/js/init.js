144b218e42f03500fde02ce409766893fa4d6910
Jackson Owens <jackson_owens@brown.edu>
2014-03-09 18:25:12 -0400

    bug fix on peernoteNS.init

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -3,23 +3,16 @@
  */
 var peernoteNS = peernoteNS || {};
 
-peernoteNS._initFunctions = [];
-
 /*
  * This function should be used in replace of $(document).ready() to
  * register functions be executed when the DOM is ready.
  */
 peernoteNS.init = function(cb) {
-  peernoteNS._initFunctions.push(cb);
-};
-
-$(document).ready(function(e) {
-  for (var i = 0; i < peernoteNS._initFunctions.length; ++i) {
+  $(document).ready(function(e) {
     try {
-      // Try to initialize the given subsystem
-      peernoteNS._initFunctions[i]();
+      cb();
     } catch(err) {
       peernoteNS.error.record(err);
     }
-  }
-});
+  });
+};
