e97b6dbbae921bf1b818ec3fb4999fd2aa4905b4
Jackson Owens <jackson_owens@brown.edu>
2014-03-10 19:06:54 -0400

    really, really simple markdown export

--- app/parsers/__init__.py
+++ app/parsers/__init__.py
@@ -1,4 +1,5 @@
 from plain_text import PlainTextParser
 from app.parsers.docx_parser import DocxParser
+from app.parsers.markdown import MarkdownParser
 
-parsers = [PlainTextParser(), DocxParser()]
+parsers = [PlainTextParser(), DocxParser(), MarkdownParser()]
