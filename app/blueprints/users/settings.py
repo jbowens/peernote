eef353c7ae9e5fcb722c54237e9210e375f47eee
Alec Lee <alec.lee15@gmail.com>
2014-03-31 19:09:43 -0400

    handle settings changes with identical data

--- app/blueprints/users/settings.py
+++ app/blueprints/users/settings.py
@@ -19,26 +19,32 @@ def settings():
         password = request.form.get('password')
         password_confirm = request.form.get('password_again')
 
-        # First name
-        if first_name and first_name != user.first_name:
-            user.first_name = first_name
-            flash('Your first name has been updated.')
+        # submitted basic info
+        if first_name and last_name and email:
+            # No actual updates
+            if email.lower() == user.email.lower() and first_name == user.first_name and last_name == user.last_name:
+                flash('Information updated')
 
-        # Last name
-        if last_name and last_name != user.last_name:
-            user.last_name = last_name
-            flash('Your last name has been updated.')
+            # First name
+            if first_name != user.first_name:
+                user.first_name = first_name
+                flash('Your first name has been updated.')
 
-        # Email
-        if email and email.lower() != user.email.lower():
-            if User.is_email_used(email):
-                flash('That email is already in use on the site.', 'error')
-            elif not validate_email(email, check_mx=True):
-                flash('That is not a valid email address', 'error')
-            else:
-                user.email = email
-                flash('Your email address has been updated.')
-        
+            # Last name
+            if last_name != user.last_name:
+                user.last_name = last_name
+                flash('Your last name has been updated.')
+
+            # Email
+            if email.lower() != user.email.lower():
+                if User.is_email_used(email):
+                    flash('That email is already in use on the site.', 'error')
+                elif not validate_email(email, check_mx=True):
+                    flash('That is not a valid email address', 'error')
+                else:
+                    user.email = email
+                    flash('Your email address has been updated.')
+
         # Password
         if password:
             if password != password_confirm:
@@ -48,5 +54,5 @@ def settings():
                 flash('Your password has been updated.')
 
         db.session.commit()
-            
+
     return render_template('users/settings.html',page_title="Settings")
