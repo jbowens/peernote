9b1362f93cd6597b1191000001c4ae82b1b32534
Alec Lee <alec.lee15@gmail.com>
2014-04-03 03:38:59 -0400

    pretty dates

--- app/models/review.py
+++ app/models/review.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from draft import Draft
 from app import db
 import user # cyclic imports suck
@@ -15,3 +15,11 @@ class Review(db.Model):
 
     def get_requesting_user(self):
         return user.User.query.filter_by(uid=self.get_draft().uid).first()
+
+    def pretty_request_date(self):
+        if self.created_date.date() == datetime.today().date():
+            return 'Today'
+        elif self.created_date.date() == datetime.today().date() - timedelta(days=1):
+            return 'Yesterday'
+        else:
+            return self.created_date.strftime('%b %d')
