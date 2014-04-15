296521f5140d8d9f59c97356e4f407405c263523
bryceAebi <bryce_aebi@brown.edu>
2014-04-15 14:20:03 -0400

    update navbar menus

--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -1,21 +1,48 @@
 $(document).ready(function() {
-    var timeout;
+    var optionsVisible = false;
+    var notificationsVisible = false;
+    var fadeInTime = 60;
+    var fadeOutTime = 100;
 
-    $('.show-dropdown').hover(function() {
-        window.clearTimeout(timeout);
-        $('.dropdown').show();
-    }, function() {
-        timeout = window.setTimeout(function() {
-            $('.dropdown').hide();
-        }, 500);
+    $('.show-dropdown').click(function(e) {
+        e.preventDefault();
+        $('.notifications-dropdown').fadeOut(fadeOutTime);
+        notificationsVisible = false;
+        if (optionsVisible) {
+            $('.options-dropdown').fadeOut(fadeOutTime);
+        } else {
+            $('.options-dropdown').fadeIn(fadeInTime);
+        }
+        optionsVisible = !optionsVisible;
     });
 
-    $('.dropdown').hover(function() {
-        window.clearTimeout(timeout);
-    }, function() {
-        timeout = window.setTimeout(function() {
-            $('.dropdown').hide();
-        }, 500);
+    $('.show-notifications').click(function(e) {
+        e.preventDefault();
+        $('.options-dropdown').fadeOut(fadeOutTime);
+        optionsVisible = false;
+        if (notificationsVisible) {
+            $('.notifications-dropdown').fadeOut(fadeOutTime);
+        } else {
+            $('.notifications-dropdown').fadeIn(fadeInTime);
+        }
+        notificationsVisible = !notificationsVisible;
+    });
+
+    $(document).click(function(e) {
+        var $target = $(e.target);
+        if (   !($target.parents(".options-dropdown").length > 0)
+            && !($target.parents(".notifications-dropdown").length > 0)
+            && !($target.parents(".show-dropdown").length > 0)
+            && !$target.hasClass("options-dropdown")
+            && !$target.hasClass("notifications-dropdown")
+            && !$target.hasClass("show-dropdown")
+            && !$target.hasClass("show-notifications")) {
+            console.log("here");
+            optionsVisible = false;
+            notificationsVisible = false;
+            $('.options-dropdown').fadeOut(fadeOutTime);
+            $('.notifications-dropdown').fadeOut(fadeOutTime);
+        }
     });
 });
 
