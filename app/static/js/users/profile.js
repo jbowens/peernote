b61260a73957542dab8ac81b82043c0fcd7a05a8
bryceAebi <bryce_aebi@brown.edu>
2014-06-20 22:49:12 -0700

    make week calendar work

--- app/static/js/users/profile.js
+++ app/static/js/users/profile.js
@@ -7,6 +7,103 @@ $.extend(peernoteNS.profile, {
         this.subscribeNotifications();
         this.lightboxInit();
         this.gradebooksInit();
+        this.initLightboxes();
+        this.initWeekCalendar();
+    },
+
+    initWeekCalendar: function() {
+        var days = {
+            0:"sunday",
+            1:"monday",
+            2:"tuesday",
+            3:"wednesday",
+            4:"thursday",
+            5:"friday",
+            6:"saturday"
+        }
+
+        // milliseconds in day
+        var milliInDay = 1000 * 60 * 60 * 24;
+
+        // today
+        var currentDate = new Date();
+
+        // get epochtime of today
+        var currentEpochTime = currentDate.getTime();
+
+        // get the 0-6 integer representing day of week
+        var currentDayNum = currentDate.getDay();
+
+        // get day of week by name
+        var currentDay = days[currentDayNum];
+
+        // keep track of which week the calendar is displaying
+        var cachedEpochTime = currentEpochTime;
+
+        // set the dates of the week
+        populateWeek(currentEpochTime);
+
+        // Given an epoch time. sets the correct dates for the other days of the week
+        function populateWeek(anchorEpochTime) {
+            var date = new Date(anchorEpochTime);
+            var anchorDayNum = date.getDay();
+            var mondayEpoch = anchorEpochTime - (anchorDayNum * milliInDay) + milliInDay;
+
+            $(".weekday").each(function() {
+                var $weekDate = $(this).find(".wd-date");
+                $weekDate.html((new Date(mondayEpoch)).getDate());
+                mondayEpoch += milliInDay;
+            });
+        }
+
+        // initialize button to switch to current week
+        $(".current-date-button").click(function() {
+            populateWeek(currentEpochTime);
+            $("."+currentDay+".weekday").addClass("current-weekday");
+            cachedEpochTime = currentEpochTime;
+        });
+
+        // initialize button to switch to next week
+        $(".next-week-button").click(function() {
+            cachedEpochTime += (7 * 24 * 60 * 60 * 1000);
+            switchWeek(cachedEpochTime);
+        });
+
+        // initialize button to switch to previous week
+        $(".previous-week-button").click(function() {
+            cachedEpochTime -= (7 * 24 * 60 * 60 * 1000);
+            switchWeek(cachedEpochTime);
+        });
+
+        // given an epoch time, switches to the week containing
+        // this time
+        function switchWeek(epochTime) {
+            populateWeek(epochTime);
+            if (epochTime === currentEpochTime) {
+                $("."+currentDay+".weekday").addClass("current-weekday");
+            }else {
+                $(".weekday").removeClass("current-weekday");
+            }
+        }
+    },
+
+    initLightboxes: function() {
+        // Initialize lightbox containing full calendar
+        var calendar = peernoteNS.widgets.initLightbox($(".calendar-popup"), {
+            closeIcon: true,
+        });
+        $(".fa-arrows-alt").click(function() {
+            calendar.open();
+        });
+
+        // Initialize lightbox for finding and adding courses
+        peernoteNS.profile.lightbox = peernoteNS.widgets.initLightbox($(".add-course-pane"), {
+            closeIcon: true,
+            onClose: peernoteNS.profile.lightboxReset
+        });
+        $(".add-course-link").click(function() {
+            peernoteNS.profile.lightbox.open();
+        });
     },
 
     // initializes the display code for each grade report
@@ -151,16 +248,5 @@ $.extend(peernoteNS.profile, {
 });
 
 peernoteNS.init(function() {
-
-    // Initialize lightbox for finding and adding courses
-    peernoteNS.profile.lightbox = peernoteNS.widgets.initLightbox($(".add-course-pane"), {
-        closeIcon: true,
-        onClose: peernoteNS.profile.lightboxReset
-    });
-
-    $(".add-course-link").click(function() {
-        peernoteNS.profile.lightbox.open();
-    });
-
     peernoteNS.profile.init();
 });
